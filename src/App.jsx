8317e86eb99d2f04acf4549a2c43be4dd90306d2
harsh2504 <harsh2504patil@gmail.com>
2024-08-16 16:50:39 +0530

    Initalized the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen w-screen ...  flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-slate-600">
+        Hello, Vite + React + Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
