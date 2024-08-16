79981fd980ced1bf9c0b5b706374007647f9d487
harsh2504 <harsh2504patil@gmail.com>
2024-08-16 17:28:28 +0530

    Checked working of tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div className="min-h-screen w-screen ...  flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen w-screen ...  flex items-center justify-center bg-gray-100 flex-col">
+    <iframe className="w-[200px] h-[200px]" src="https://lottie.host/embed/e8b2a65f-f3d2-48f7-9f36-4328a92b2866/bvW5TcBO28.json"></iframe>
       <h1 className="text-4xl font-bold text-slate-600">
-        Hello, Vite + React + Tailwind CSS!
+        Hello, Folks !!
       </h1>
+   <h5 className="text-xl font-medium text-slate-400">Let's build this stuff together</h5>
     </div>
   );
 }
